12253b4cbc413d48baa89641d3f13139ee50f606
rezaharrist <81601774+rezaharrist@users.noreply.github.com>
2021-05-02 19:31:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uts_reza_311810033_reza/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.uts_reza_311810033_reza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
